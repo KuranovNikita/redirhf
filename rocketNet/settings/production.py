414c19426c40564fa8a98d5c2aaa7db907457cee
NikitaK <kuranovnikita@gmail.com>
2021-12-17 11:43:55 +0400

    'fix2'

--- rocketNet/settings/production.py
+++ rocketNet/settings/production.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s#0c^1#jf2%tj33zh*w$v)2lph*r747$^s)wed)hv)%aj7fe0m'
+# SECRET_KEY = 'django-insecure-s#0c^1#jf2%tj33zh*w$v)2lph*r747$^s)wed)hv)%aj7fe0m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
